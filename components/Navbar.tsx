52d571e62265f390199ba2f05db498e4ab6ab28e
Hero-Codes <omagrawal141207@gmail.com>
2024-01-06 21:42:27 +0530

    almost done with everything

---
+++ components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { SignedIn, SignedOut, UserButton, SignUpButton, SignInButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+export const Navbar = () => {
+    return (
+        <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+            <Link href="/" className="flex items-center gap-x-4">
+                <Image
+                    src={"/logo.jpeg"}
+                    alt=".."
+                    height={30}
+                    width={30} />
+            </Link>
+            <div className="ml-auto flex items-center gap-x-2">
+                <UserButton
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: {
+                                height: 30,
+                                width: 30,
+                            }
+                        }
+                    }}
+                />
+
+                <SignedIn>
+                    <div className="flex gap-x-1 items-center">
+                        <Link href="/profile">
+                            <Button size={"sm"}>Profile</Button>
+                        </Link>
+
+                        <Link href="/topics">
+                            <Button variant="secondary" size="sm">Topics</Button>
+                        </Link>
+                    </div>
+
+                </SignedIn>
+
+                <SignedOut>
+                    <SignInButton mode="modal">
+                        <Button size={"sm"} variant={"outline"}>Sign In</Button>
+                    </SignInButton>
+
+                    <SignUpButton mode="modal">
+                        <Button size={"sm"}>Get Started</Button>
+                    </SignUpButton>
+                </SignedOut>
+            </div>
+        </nav>
+    );
+};
